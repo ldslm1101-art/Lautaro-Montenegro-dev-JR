d186acb97a7a30e212cf3c321f488c8833b76cb7
ldslm1101-art <lds.lm1101@gmail.com>
2026-03-11 02:11:39 -0300

    CAMBIO

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/Lautaro-Montenegro-dev-JR/",
+  base: '/Lautaro-Montenegro-dev-JR/',
   plugins: [react()],
   resolve: {
     alias: {
