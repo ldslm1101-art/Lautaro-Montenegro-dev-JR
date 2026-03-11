67306af7db4faa4980aca6df7e0331ead3a284c4
ldslm1101-art <lds.lm1101@gmail.com>
2026-03-11 02:21:45 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
 
-// Archivo: vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/Lautaro-Montenegro-dev-JR/', //
+  // El nombre debe ser idéntico al que pusiste en GitHub
+  base: '/Lautaro-Montenegro-dev-JR/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
