ac319b8a116c98b4a2ce98b5c7d547eb7b0b47a3
ldslm1101-art <lds.lm1101@gmail.com>
2026-03-13 23:46:39 -0300

    fin.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 // vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    // Cambiamos a minúsculas para que coincida con la URL real de GitHub
-    base: command === 'serve' ? '/' : '/lautaro-montenegro-dev-jr/', 
+    // Al usar './', la página funcionará sin importar el nombre del repo
+    base: command === 'serve' ? '/' : './', 
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
