6ec863f899cd47b70af2fc87e62e5c7bde01d824
ldslm1101-art <lds.lm1101@gmail.com>
2026-03-13 22:00:41 -0300

    3er commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/Lautaro-Montenegro-dev-JR/",
   plugins: [react()],
   resolve: {
     alias: {
